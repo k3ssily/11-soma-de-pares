73f7bbf7398a30a8d6959d62c90af413bb316e06
k3ssily <cursoprozzz@gmail.com>
2026-03-19 20:54:45 -0300

    Add files via upload

---
+++ soma de pares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	  
+	  int soma = 0;
+	  
+	  for(int i= 1; i <= 100; i++){
+	  
+	  if(i % 2==0 ){
+	   soma +=i;
+	  
+	 
+	
+	}
+}
+    System.out.println("a soma é:" + soma);
+}
+}
